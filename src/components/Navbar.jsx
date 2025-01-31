b5f19d3df28486d97238db5cd8a7fda7adb9dd50
sathishsrini <sathishsrini499@gmail.com>
2025-01-31 23:08:19 +0530

    profipic udiu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import logo from "../assets/l.png";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import resume from "../assets/komathi.pdf";
 
 
 const Navbar = () => {
+  const handleResumeClick = () => {
+    // Open the resume in a new tab
+    window.open(resume, "_blank");
+  };
   return (
     <nav className="mb-5 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         <img className="mx-2 ml-14 w-10" src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        {/* Resume Button */}
+        <button
+          onClick={handleResumeClick}
+          className="cursor-pointer bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-300"
+        >
+          Resume
+        </button>
         <FaLinkedin
           onClick={() =>
             (window.location.href =
